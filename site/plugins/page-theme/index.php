8cd5acc21645f82abbbddb0a950e45c4396680f2
Matthias Deckx <info@matthiasdeckx.studio>
2026-07-07 19:46:19 +0200

    remove page_theme option from some pages

--- site/plugins/page-theme/index.php
+++ site/plugins/page-theme/index.php
@@ -5,13 +5,23 @@ use Kirby\Cms\App as Kirby;
 return Kirby::plugin('site/page-theme', [
   'pageMethods' => [
     'usesLightTheme' => function () {
+      $template = $this->intendedTemplate()->name();
+
+      if (in_array($template, ['news', 'news-article'], true)) {
+        return true;
+      }
+
+      if ($template === 'project') {
+        return false;
+      }
+
       if ($this->page_theme()->isNotEmpty()) {
         return $this->page_theme()->value() === 'light';
       }
 
       return in_array(
         $this->intendedTemplate()->name(),
-        ['work', 'news', 'news-article', 'creators'],
+        ['work', 'creators'],
         true
       );
     },
